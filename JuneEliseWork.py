d9653001f5a4f0e81bbd0040676dd28c527a49b5
Antonios Glampedakis <antonios.glampedakis@ascential.com>
2021-11-11 15:35:52 +0000

    adding elise work

--- JuneEliseWork.py
+++ JuneEliseWork.py
@@ -5,23 +5,24 @@ from imports import *
 
 #this covers excels no 1, 10,11,13,14
 def createPivotTables(df,listOfIndex):
-    listForNewDf = listOfIndex
+    listForNewDf = copy.deepcopy(listOfIndex)
     listOfIndex.append("FestivalYear")
-    dfAll = df.groupby(listForNewDf)["All Entries"].count().reset_index()
-    dfShortlisted = df.groupby(listForNewDf)["Shortlist"].count().reset_index()
-    dfWinners = df.groupby(listForNewDf)["Winner"].count().reset_index()
+    dfAll = df.groupby(listOfIndex)["All Entries"].count().reset_index().pivot("All Entries",listForNewDf, "FestivalYear")
+    dfShortlisted = df.groupby(listOfIndex)["Shortlist"].count().reset_index().pivot("Shortlist",listForNewDf, "FestivalYear")
+    dfWinners = df.groupby(listOfIndex)["Winner"].count().reset_index().pivot("Winner",listForNewDf, "FestivalYear")
 
     dfAllPerc = getPercDf(dfAll)
     dfShortlistedPerc = getPercDf(dfShortlisted)
     dfWinnersPerc = getPercDf(dfWinners)
-    
 
+    return dfAllPerc
 
 
 def getPercDf(df):
     dfNew = pd.DataFrame()
+
     for column in df:
-        print(column)
         dfNew[getVariableName(column)] = df[column] / df[column].sum()
-        print(df.head())
     return dfNew
+
+skata = createPivotTables(df, ["Country"])
